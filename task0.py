30ccf5b4eddbf789ee16ebd8dd7dad75901a74f9
Sai Pavan Kumar Vanamala <vanamala@Sais-MacBook-Air.local>
2022-09-22 14:16:15 -0300

    second commit

---
+++ task0.py
@@ -0,0 +1,9 @@
+for num in range(1,101):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz: "+str(num))
+    elif num % 3 == 0:
+        print("Fizz:" +str(num))
+    elif num % 5 == 0:
+        print("Buzz:" +str(num))
+    else:
+        print(num)
